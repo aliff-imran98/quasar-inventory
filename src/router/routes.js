2a2c9b09a2b501d1c382cfacefd858c812d0c9e2
aliff-imran98 <58795206+aliff-imran98@users.noreply.github.com>
2023-11-24 15:09:00 +0800

    add route for each page

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,37 @@
 const routes = [
   {
     path: "/",
+    name: "HomePage",
     component: () => import("layouts/MainLayout.vue"),
     children: [{ path: "", component: () => import("src/pages/HomePage.vue") }],
+    children: [
+      {
+        path: "/cpu",
+        name: "cpu",
+        component: () => import("src/pages/cpu/CpuPage.vue"),
+      },
+      {
+        path: "/cpuCreate",
+        name: "createCpu",
+        component: () => import("src/pages/cpu/CpuCreate.vue"),
+      },
+      {
+        path: "/cpuEdit/:id",
+        name: "edit",
+        component: () => import("src/pages/cpu/CpuEdit.vue"),
+      },
+    ],
+  },
+
+  {
+    path: "/register",
+    component: () => import("src/pages/RegisterPage.vue"),
+  },
+
+  {
+    path: "/login",
+    name: "LoginPage",
+    component: () => import("src/pages/LoginPage.vue"),
   },
 
   // Always leave this as last one,
