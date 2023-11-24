088558b72d14d553248664c296d9f1553422b9c7
aliff-imran98 <58795206+aliff-imran98@users.noreply.github.com>
2023-11-24 15:08:19 +0800

    add cpu edit page

---
+++ src/pages/cpu/CpuEdit.vue
@@ -0,0 +1,81 @@
+<!-- src/pages/EditPage.vue -->
+<template>
+  <q-page>
+    <q-container>
+      <q-form @submit="updateCpu">
+        <q-input v-model="cpuData.cpu_name" label="CPU Name" />
+        <q-input v-model="cpuData.cpu_brand" label="CPU Brand" />
+        <q-input v-model="cpuData.cpu_cost" label="CPU Cost" type="number" />
+        <q-input v-model="cpuData.cpu_serial" label="CPU Serial" />
+        <q-btn type="submit" label="Update CPU" color="primary" />
+      </q-form>
+    </q-container>
+  </q-page>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import supabaseInit from "src/boot/supabase";
+import { useRouter } from "vue-router";
+
+export default {
+  setup() {
+    const cpuData = ref({
+      cpu_name: "",
+      cpu_brand: "",
+      cpu_cost: 0,
+      cpu_serial: "",
+    });
+
+    const supabase = supabaseInit;
+    const router = useRouter();
+    const cpuId = router.currentRoute.value.params.id;
+
+    const updateCpu = async () => {
+      try {
+        console.log("Updating CPU with data:", cpuData.value);
+        const { data, error } = await supabase
+          .from("Cpu")
+          .update({
+            cpu_name: cpuData.value.cpu_name,
+            cpu_brand: cpuData.value.cpu_brand,
+            cpu_cost: cpuData.value.cpu_cost,
+            cpu_serial: cpuData.value.cpu_serial,
+          })
+          .eq("id", cpuId);
+
+        if (error) {
+          console.error("Error updating CPU:", error.message);
+        } else {
+          console.log("CPU updated successfully:", data);
+          // Redirect to the home page
+          router.push({ name: "cpu" });
+        }
+      } catch (error) {
+        console.error("Update error:", error.message);
+      }
+    };
+
+    onMounted(async () => {
+      // Fetch CPU data based on the ID from the route parameter
+      const { data, error } = await supabase
+        .from("Cpu")
+        .select("*")
+        .eq("id", cpuId)
+        .single();
+
+      if (error) {
+        console.error("Error fetching CPU data:", error.message);
+      } else {
+        console.log("Fetched CPU data:", data);
+        cpuData.value = data;
+      }
+    });
+
+    return {
+      cpuData,
+      updateCpu,
+    };
+  },
+};
+</script>
