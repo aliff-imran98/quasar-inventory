57b5db344bb7c945edc810ee4a7c779dd9aa8839
aliff-imran98 <58795206+aliff-imran98@users.noreply.github.com>
2023-11-26 01:53:43 +0800

    add route for mobo and gpu pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,37 +1,129 @@
 const routes = [
   {
     path: "/",
-    name: "HomePage",
+    meta: {
+      title: "Home",
+      auth: true,
+    },
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("src/pages/HomePage.vue") }],
     children: [
+      {
+        path: "/",
+        name: "HomePage",
+        meta: {
+          title: "Home",
+          auth: true,
+        },
+        component: () => import("src/pages/HomePage.vue"),
+      },
       {
         path: "/cpu",
         name: "cpu",
+        meta: {
+          title: "Cpu",
+          auth: true,
+        },
         component: () => import("src/pages/cpu/CpuPage.vue"),
       },
       {
         path: "/cpuCreate",
         name: "createCpu",
+        meta: {
+          title: "Create Cpu",
+          auth: true,
+        },
         component: () => import("src/pages/cpu/CpuCreate.vue"),
       },
       {
         path: "/cpuEdit/:id",
-        name: "edit",
+        name: "cpuEdit",
+        meta: {
+          title: " Edit Cpu",
+          auth: true,
+        },
         component: () => import("src/pages/cpu/CpuEdit.vue"),
+      },
+      {
+        path: "/mobo",
+        name: "mobo",
+        meta: {
+          title: "MotherBoard",
+          auth: true,
+        },
+        component: () => import("src/pages/mobo/MoboPage.vue"),
+      },
+      {
+        path: "/moboCreate",
+        name: "createMobo",
+        meta: {
+          title: "Create MotherBoard",
+          auth: true,
+        },
+        component: () => import("src/pages/mobo/MoboCreate.vue"),
+      },
+      {
+        path: "/moboEdit/:id",
+        name: "moboEdit",
+        meta: {
+          title: "Edit MotherBoard",
+          auth: true,
+        },
+        component: () => import("src/pages/mobo/MoboEdit.vue"),
+      },
+      {
+        path: "/gpu",
+        name: "gpu",
+        meta: {
+          title: "Gpu",
+          auth: true,
+        },
+        component: () => import("src/pages/gpu/GpuPage.vue"),
+      },
+      {
+        path: "/gpuCreate",
+        name: "createGpu",
+        meta: {
+          title: "Create MotherBoard",
+          auth: true,
+        },
+        component: () => import("src/pages/gpu/GpuCreate.vue"),
+      },
+      {
+        path: "/gpuEdit/:id",
+        name: "gpuEdit",
+        meta: {
+          title: "Edit MotherBoard",
+          auth: true,
+        },
+        component: () => import("src/pages/gpu/GpuEdit.vue"),
       },
     ],
   },
 
   {
-    path: "/register",
-    component: () => import("src/pages/RegisterPage.vue"),
-  },
-
-  {
-    path: "/login",
-    name: "LoginPage",
-    component: () => import("src/pages/LoginPage.vue"),
+    path: "/auth",
+    auth: false,
+    component: () => import("src/layouts/BlankLayout.vue"),
+    children: [
+      {
+        path: "register",
+        name: "RegisterPage",
+        meta: {
+          title: "Register",
+          auth: false,
+        },
+        component: () => import("src/pages/RegisterPage.vue"),
+      },
+      {
+        path: "login",
+        name: "LoginPage",
+        meta: {
+          title: "Register",
+          auth: false,
+        },
+        component: () => import("src/pages/LoginPage.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
